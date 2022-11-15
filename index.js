9cea742fb20f46f6f31a9b04e0b61acb574d2d4d
Ole Voss <jan-ole.voss@stud.hs-emden-leer.de>
2022-11-15 11:59:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import 'bulma/css/bulma.css';
+import sha256 from './node_modules/js-sha256';
+import zxcvbn from './node_modules/zxcvbn';
+
+document.getElementById ("generate").addEventListener ("click", generate, false);
+
+function generate(){
+    var website = document.getElementById("website").value;
+    var key = document.getElementById("key").value;
+    var length = document.getElementById("length").value;
+
+    var passwordField = document.getElementById("passwordField");
+    var timeToCrackField = document.getElementById("timeToCrack");
+    var passwordScoreBar = document.getElementById("passwordScoreBar");
+
+    var result = sha256.hmac(website, key);
+    var resultFormatted = result.slice(0, length);
+    var data = zxcvbn(resultFormatted);
+    
+    passwordField.innerHTML = resultFormatted
+    passwordScoreBar.value = data.score;
+    if (data.score == 1){
+        passwordScoreBar.className = "progress is-danger";
+    }
+    else if (data.score == 2){
+        passwordScoreBar.className = "progress is-warning";
+    }
+    else if (data.score == 3){
+        passwordScoreBar.className = "progress is-info";
+    }
+    else if (data.score == 4){
+        passwordScoreBar.className = "progress is-success";
+    }
+    timeToCrackField.innerHTML = data.crack_times_display.offline_slow_hashing_1e4_per_second;
+}
