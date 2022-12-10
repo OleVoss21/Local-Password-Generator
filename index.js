3d3622f2de91079d4869c71658ca53df8606ada3
Ole Voss <jan-ole.voss@stud.hs-emden-leer.de>
2022-12-10 16:25:17 +0100

    Canonising website url

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import zxcvbn from './node_modules/zxcvbn';
 document.getElementById ("generate").addEventListener ("click", generate, false);
 
 function generate(){
+    //init parser
+    var parser = document.createElement("a");
+
     var website = document.getElementById("website").value;
     var key = document.getElementById("key").value;
     var length = document.getElementById("length").value;
@@ -15,16 +18,27 @@ function generate(){
     var timeToCrackField = document.getElementById("timeToCrack");
     var passwordScoreBar = document.getElementById("passwordScoreBar");
     var passwordScoreBarDiv = document.getElementById("passwordScoreBarDiv");
+    var generatedPasswordFor = document.getElementById("generatedPasswordFor");
 
+    //parse URL and extract domain
+    parser.href = website;
+    if(!(parser.hostname == 'localhost')){
+        website = parser.hostname;
+    }
+
+    //calculate and format hash
     var result = sha256.hmac(website, key);
     var resultFormatted = result.slice(0, length);
     var data = zxcvbn(resultFormatted);
     
+    //fill elements with results
+    generatedPasswordFor.innerHTML = "Generated password for " + website + ":" 
     passwordField.innerHTML = resultFormatted
     passwordScoreBar.value = data.score;
     passwordScoreBarDiv.setAttribute('data-tooltip', data.score + '/4');
     timeToCrackField.innerHTML = data.crack_times_display.offline_slow_hashing_1e4_per_second;
 
+    //rank password score
     if (data.score == 1){
         passwordScoreBar.className = "progress is-danger";
     }
